c61f5814bea9e980b89af5d15dc451c26807d440
Your Name <you@example.com>
2023-04-04 17:06:55 +0200

    vite build change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,19 @@ dns.setDefaultResultOrder("verbatim");
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
